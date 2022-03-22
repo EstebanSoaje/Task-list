1a11a3acdf2a893c56ecb0a8db016e4e9b2f0164
estebansoaje <esferreres@gmail.com>
2022-03-22 18:45:37 -0300

    Lista de tareas completa

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,7 +2,6 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -20,5 +19,27 @@ export class TasksComponent implements OnInit {
       this.taskService.getTasks().subscribe((tasks) => (
         this.tasks = tasks
       ));
+    }
+
+    deleteTask(task: Task) {
+      this.taskService.deleteTask(task)
+      .subscribe(
+        (): void=>{
+          this.tasks = this.tasks.filter( (t) => {
+          return t.id !== task.id
+        })
+      })
+    }
+
+    toggleReminder(task: Task) {
+      task.reminder = !task.reminder
+      this.taskService.updateTaskReminder(task).subscribe();
+    }
+
+    
+    addTask(task: Task){
+      this.taskService.addTask(task).subscribe((task) => (
+      this.tasks.push(task)
+      ))
     }
   }
